bf1e3cfd9cb161bf1736edc6c334502b38e84d14
dawntepper <dawn.dtepdc@gmail.com>
2024-10-21 21:33:00 -0400

    Updates to global.css styles for todo form

--- rollup.config.js
+++ rollup.config.js
@@ -7,6 +7,7 @@ import sveltePreprocess from 'svelte-preprocess';
 import typescript from '@rollup/plugin-typescript';
 import css from 'rollup-plugin-css-only';
 import copy from 'rollup-plugin-copy';
+import postcss from 'rollup-plugin-postcss';
 
 const production = !process.env.ROLLUP_WATCH;
 
@@ -32,28 +33,39 @@ function serve() {
 }
 
 export default {
-    input: 'src/background.js',
+    input: 'src/main.js',
     output: {
-      sourcemap: true,
-      format: 'iife',
-      name: 'background',
-      file: 'dist/build/background.js'
+        sourcemap: true,
+        format: 'iife',
+        name: 'app',
+        file: 'dist/build/bundle.js'
     },
     plugins: [
         svelte({
-            preprocess: sveltePreprocess({ sourceMap: !production }),
+            preprocess: sveltePreprocess({ 
+                sourceMap: !production,
+                postcss: true
+            }),
             compilerOptions: {
                 dev: !production
             }
         }),
+        postcss({
+            extract: 'bundle.css',
+            minimize: production,
+            use: [
+                ['sass', {
+                    includePaths: ['./src/theme', './node_modules']
+                }]
+            ]
+        }),
         copy({
             targets: [
-              { src: 'src/index.html', dest: 'dist' },
-              { src: 'src/manifest.json', dest: 'dist' },
-              { src: 'src/*.png', dest: 'dist' }  // This will copy all PNG files (like your icons)
+                { src: 'src/index.html', dest: 'dist' },
+                { src: 'src/manifest.json', dest: 'dist' },
+                { src: 'src/*.png', dest: 'dist' }
             ]
-          }),
-        css({ output: 'build/bundle.css' }),
+        }),
         resolve({
             browser: true,
             dedupe: ['svelte']
